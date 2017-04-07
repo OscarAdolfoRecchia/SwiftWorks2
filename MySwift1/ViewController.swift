b0c68a83c1f0abc45d1a2fd0744d5fe81de803f0
Oscar Recchia <oscaradolforecchia@Oscars-MacBook-Pro.local>
2017-04-07 13:53:54 +0200

    Initial Commit

---
+++ MySwift1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySwift1
+//
+//  Created by Oscar Adolfo Recchia on 4/7/17.
+//  Copyright © 2017 Oscar Adolfo Recchia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
