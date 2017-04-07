8e54b2c71b1aee00bdf4f2c1d44ecaf3beaa12fc
Oscar Recchia <oscaradolforecchia@Oscars-MacBook-Pro.local>
2017-04-07 15:57:09 +0200

    Ambiente test closures

--- MySwift1/AppDelegate.swift
+++ MySwift1/AppDelegate.swift
@@ -12,16 +12,68 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    
+    
+    var myFloat : Float = 0.0
+    
+    
+    
+    var myk : Int = 5
+    
+    
+    
+    func applyKTimes(f:(Float) -> Float,x:Float,k:Int) -> Float
+    {
+        
+        var fff : Float = x
+        
+        print ( "Sono all'interno")
+        
+       for _  in 0...k-1
+       {
+        
+        print ( fff)
+        fff =  f(fff)
+        
+        }
+        
+        return fff         }
+    
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+               print ("Sono qui")
+        
+        
+        
+        print ( "Sono in application")
+        
+        myFloat = applyKTimes(f:{ $0 + 23 /* every kind of processing */ }, x: myFloat, k: myk)
+
+print (" Final  myFloat   = \(myFloat)")
         // Override point for customization after application launch.
         return true
     }
+    
+    
+    func applyTwice(f : (Float) -> Float, x: Float) -> Float {
+        print (" ciaoooooo")
+        return (  f(f(x)))
+        
+    }
+    
+    
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        print ("Sono qui")
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
